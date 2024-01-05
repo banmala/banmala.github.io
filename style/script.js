fbbd333a68d25a015a803ca1ce430659253b02b9
banmala <banmalasunil@gmail.com>
2024-01-05 13:08:56 +0545

    test tech jar website

---
+++ style/script.js
@@ -0,0 +1,9 @@
+// Function to toggle the nav bar
+// function toggleNav() {
+//     let x = document.getElementById("menus");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
---
+++ style/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#008c82', //theme color
+          fblack: '#252B2E', //false black color in some headings
+          gblack: '#6E6E6E',  //grayed black color for some paragraphs
+
+
+        }
+      }
+    }
+}
