45f3c432ffc7f5e0fe86937a668441f2a1bd3866
banmala <banmalasunil@gmail.com>
2024-01-09 12:21:19 +0545

    test jan9

--- style/tailwind.config.js
+++ style/tailwind.config.js
@@ -5,9 +5,12 @@ tailwind.config = {
           primary: '#008c82', //theme color
           fblack: '#252B2E', //false black color in some headings
           gblack: '#6E6E6E',  //grayed black color for some paragraphs
+          secondary: 'rgba(0, 140, 130, 0.1)' //#008c82 with 10% opacity
 
-
-        }
-      }
+        },
+        fontFamily: {
+          'lex':['"Lexend"']
+        },
+      },
     }
 }
